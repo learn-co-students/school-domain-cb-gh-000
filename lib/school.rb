c959cde4a6b4bddd4a537b584d4bfd099e8f1de2
Trevor Watson <wtrevor162@gmail.com>
2017-02-09 02:42:12 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,37 @@
-# code here!
+class School
+
+  attr_accessor :roser
+  attr_reader :roster
+
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+  def add_student(name, grade)
+    if roster.include?(grade)
+      roster[grade] << name
+    else
+      roster[grade] = [name]
+    end
+  end
+
+  def grade(grade)
+    roster[grade]
+  end
+
+  def sort
+    sorted = {}
+    roster.sort.map do |grade, students|
+      sorted[grade] = students.sort
+    end
+    sorted
+  end
+
+end
+
+{
+  9 => ["Bart Simpson", "Homer Simpson"],
+  10 => ["Jeff Baird", "Avi Flombaum"],
+  7 => ["Jack Bauer", "Blake Johnson"],
+}
