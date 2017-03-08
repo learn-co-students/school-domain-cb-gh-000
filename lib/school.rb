033e695a2dd9cc0395d173f1ed13410f70b898ec
Dan Strong <dangaston@gmail.com>
2017-03-08 07:51:58 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,32 @@
-# code here!
+class School
+
+  attr_accessor :student, :grade, :roster
+
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+  # add_student is able to add a student, add multiple students to a grade, add students to different grades
+  def add_student(student, grade)
+    if @roster[grade] == nil
+        @roster[grade] = []
+        @roster[grade] << student
+    else
+      @roster[grade] << student
+    end
+  end
+
+  # grade is able to retrieve students from a grade
+  def grade(grade)
+    return @roster[grade]
+  end
+
+  # sort is able to sort the students
+  def sort
+    @roster.each do |key, value|
+      value = value.sort!
+    end
+  end
+  @roster
+end
