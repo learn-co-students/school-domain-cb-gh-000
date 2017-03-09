a6dbb6d7c38167addcaf54d472487dbeb635948e
Karl Jahn <karl0101@gmail.com>
2017-03-09 20:55:48 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,28 @@
+require "pry"
 # code here!
+class School
+
+  attr_reader :roster
+
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+  def add_student(name, grade)
+    @roster[grade] = [] if @roster[grade] == nil
+    @roster[grade] << name
+  end
+
+  def grade(grade)
+    @roster[grade]
+  end
+
+  def sort
+    for key in @roster.keys
+      @roster[key] = @roster[key].sort
+    end
+    @roster
+  end
+
+end
