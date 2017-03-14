50ef23571b80dbe564ab07a7f89d497c73e4437e
LuisRodriguez <CodeKommissar@gmail.com>
2017-03-13 20:35:29 -0400

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,33 @@
 # code here!
+class School
+	def initialize(name)
+		@name = name
+		@roster = {}
+	end
+
+	def roster
+		@roster
+	end
+
+	def add_student(student_name, grade)
+		if @roster.key?(grade) == false
+			@roster[grade] = []
+			@roster[grade] << student_name	
+		else 
+			@roster[grade] << student_name
+		end
+		
+		puts @roster
+	end
+
+	def grade(grade_key)
+		@roster[grade_key]
+	end
+
+	def sort
+		@roster.map { |k,v| [k,v.sort] }.to_h
+	end
+end
+
+#lol = School.new("lol")
+#puts lol.roster
