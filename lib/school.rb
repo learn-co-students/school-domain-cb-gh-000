c7b077f0f1fb1f802cc3b365cff4eb443414dee0
Adrian Daras <getpingui@gmail.com>
2017-03-15 20:52:00 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,21 @@
-# code here!
+class School
+  attr_reader :roster
+
+  def initialize(title)
+    @roster = {}
+    @title = title
+  end
+
+  def add_student(name, grade)
+    @roster[grade] = [] if !@roster.key?(grade)
+    @roster[grade] << name
+  end
+
+  def grade(key)
+    @roster[key]
+  end
+
+  def sort
+    @roster.map { |grade, students| [grade, students.sort] }.to_h
+  end
+end
