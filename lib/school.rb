65cec508629c1d42200375bfcf7b32c165a7f627
Sinan Comert <sinancomert@std.sehir.edu.tr>
2017-04-19 14:54:01 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,36 @@
 # code here!
+
+class School
+
+  attr_accessor :roster
+  attr_accessor :name
+   
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+  def add_student(name, grade)
+    if @roster.has_key?(grade)
+      @roster[grade] << name
+    else
+      @roster[grade] = []
+      @roster[grade] << name
+    end
+  end
+
+  def grade(number)
+    if @roster.has_key?(number)
+      @roster[number]
+    end
+  end
+
+  def sort()
+    @roster.each{|key,value| @roster[key] = value.sort!} 
+    @roster
+  end
+end
+
+s = School.new("deneme")
+s.add_student("sinan", 5)
+puts s.roster
