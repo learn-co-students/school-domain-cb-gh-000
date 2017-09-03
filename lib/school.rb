68ae3c9b47e04fc3ba9b4a9b7c79857872447e48
Hossam Hosny <hossam.hosni.95@gmail.com>
2017-09-03 23:42:32 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,35 @@
 # code here!
+class School
+	def initialize (name)
+		@name = name
+		@roster = {}
+	end
+
+	def roster=(hash)
+		@roster = hash
+	end
+
+	def roster
+		@roster
+	end
+
+	def add_student(name, grade)
+		if @roster.keys.include?(grade)
+			@roster[grade] << name
+		else
+			@roster[grade] = []
+			@roster[grade] << name
+		end
+	end
+
+	def grade(grade)
+		@roster[grade]
+	end
+
+	def sort
+		@roster.each do |grade, students|
+			students.sort!
+		end
+	end
+
+end
