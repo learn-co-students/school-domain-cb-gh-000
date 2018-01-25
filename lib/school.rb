4818804776d8b5fdb45e34924c0e979eb7ecd442
Kyle Harris <k_harris95@live.com>
2018-01-25 06:58:51 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,22 @@
 # code here!
+class School
+  attr_accessor :name, :roster
+
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+  def add_student(student, grade)
+    students = @roster[grade]
+    @roster[grade] = students ? students << student : [student]
+  end
+
+  def grade(score)
+    @roster[score]
+  end
+
+  def sort
+    @roster.sort.map { |e| [e[0], e[1].sort] }.to_h
+  end
+end
