d6a8d15c5d03b7b310e4177357220725c1f93817
Harsh Vardhan <thvardhan.god@gmail.com>
2018-03-11 06:20:31 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,28 @@
 # code here!
+class School
+
+  def initialize name
+    @roster={}
+    @name=name
+  end
+
+  def roster
+    @roster
+  end
+
+  def add_student name,grade
+    @roster[grade]=[] if not @roster.keys.include?(grade)
+    @roster[grade]<<name
+  end
+
+  def grade grade
+    @roster[grade]
+  end
+
+  def sort
+    @roster.each do |grade,name_arr|
+      name_arr.sort!
+    end
+  end
+
+end
