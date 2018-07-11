b7d8bbaf8b3e5eb2a074be5d71e66cf38cac602a
Ertugrul Dal <ertugrul.dal@ogr.sakarya.edu.tr>
2018-07-11 02:51:04 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,38 @@
 # code here!
+class School
+
+  attr_accessor :name, :roster
+
+  def initialize(name)
+     @name = name
+     @roster = {}
+  end
+
+  def roster
+    @roster
+  end
+
+  def add_student(student_name, grade)
+    @roster[grade] ||= []
+    @roster[grade] << student_name
+
+  end
+  end
+
+  def grade(grade)
+    @roster.map do |student_grade, student_names|
+       if grade == student_grade
+         return student_names
+    end
+  end
+
+end
+
+  def sort
+    @sort_hash = {}
+     @roster.each do |student_grade_sort, student_names_sort|
+        sort_hash[student_grade_sort] = student_names_sort.sort
+     end
+     sort_hash
+
+end
