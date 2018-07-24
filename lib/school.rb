cf235e804dbb0f1bd740e3e3183069904e28cb8c
Roberto Garza <robertg4567@hotmail.com>
2018-07-24 18:51:12 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,36 @@
-# code here!
+class School
+  #attr_accessor
+  attr_reader :name, :roster
+
+  def initialize(name)
+    @name = name
+    roster = {}
+    @roster = roster
+  end
+
+  def add_student(name, grade)
+    if roster.include?(grade)
+      roster[grade] << name
+    else
+      roster[grade] = [] #create new key in hash
+      roster[grade] << name
+    end
+  end
+
+  def grade(customGrade)
+    roster.each do |rosterGrade, name|
+      if customGrade == rosterGrade
+        return name
+      end
+    end
+  end
+
+  def sort
+    sorted = {}
+    roster.each do |grade, names|
+      sorted[grade] = names.sort
+    end
+    sorted
+  end
+
+end
