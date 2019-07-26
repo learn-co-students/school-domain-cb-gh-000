b930a93a465cbc4d3d28663cd045f5f9e40f426c
Carmen Maymo <mmaymo2@alumno.uned.es>
2019-07-26 07:03:06 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,33 @@
 # code here!
+class School
+
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+  def roster
+    @roster
+  end
+  def add_student(studentname, grade)
+    @studentname = studentname
+    @grade = grade
+
+    if @roster.key?(grade)
+
+      @roster[grade] << studentname
+    else
+      @roster[grade] = []
+      @roster[grade] << studentname
+    end
+  end
+  def grade(grade)
+    @roster[grade]
+  end
+  def sort
+    newHash = {}
+    @roster.each do |grade, names|
+      newHash[grade]= []
+      newHash[grade] << [names.sort!]
+    end
+  end
+end
