c71bd5294847c717b515ae5f3b3d8c4941300fed
Sotiris Kavvouras <sotos.kavvouras@gmail.com>
2019-09-11 00:54:37 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,30 @@
-# code here!
+class School
+
+  attr_reader :name
+  attr_accessor :roster
+
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+  def add_student(name, grade)
+    if !@roster.keys.include?(grade)
+      @roster[grade] = []
+    end
+    @roster[grade] << name
+  end
+
+  def grade(grade)
+    @roster[grade]
+  end
+
+  def sort
+    @roster.each do |grade, names|
+      names.sort do |a , b|
+        a <=> b
+      end
+    end
+  end
+
+end
